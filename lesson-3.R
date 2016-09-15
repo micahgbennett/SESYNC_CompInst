e5e30672df8c1e318018bf4222765a25d7fe3806
Bennett <Bennett.Micah@epa.gov>
2016-09-15 06:35:58 -0400

    changes to lesson 3 9/15

--- lesson-3.R
+++ lesson-3.R
@@ -1,7 +1,28 @@
+#from PPT
+install.packages("RPostgreSQL",loc="C:/Users/mbennett/Desktop/Rlibs2")
+library(RPostgreSQL,lib.loc="C:/Users/mbennett/Desktop/Rlibs2")
+
+con <- dbConnect(SQLite(), "data/portal.sqlite")
+dbListTables(con)
+#
+
+#2 ways to get/read data
+#import into R and use functions 
+plots <- dbReadTable(con, "plots")
+surveys <- dbReadTable(con, "surveys")
+species <- dbReadTable(con, "species")
+str(surveys)
+
+##OR let database system process for you
+dbGetQuery(con, "select species_id, weight
+                 from surveys
+                 where plot_id = 1 limit 5")
+
+
 # Database connections from R
 
-library(RSQLite)
-con <- dbConnect(...)
+library(RSQLite, lib.loc="C:/Users/mbennett/Desktop/Rlibs2")
+con <- dbConnect(SQLite(), "data/portal.sqlite")
 dbGetQuery(con, "pragma foreign_keys = on")
 ...
 
